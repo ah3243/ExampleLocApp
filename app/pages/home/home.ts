a31e0a7afd19ed3d475817ed15ee18751423e9a0
albert <1c2fffe2@opayq.com>
2016-09-11 16:17:15 +0100

    initial commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,38 @@
+// import { Component } from '@angular/core';
+// import { NavController } from 'ionic-angular';
+
+// @Component({
+//   templateUrl: 'build/pages/home/home.html'
+// })
+// export class HomePage {
+//   constructor(public navCtrl: NavController) {
+//   }
+
+// }
+
+import {Page} from 'ionic-angular';
+import {Camera} from 'ionic-native';
+
+@Page({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  public base64Image: string;
+
+  constructor() {
+
+  }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+}
